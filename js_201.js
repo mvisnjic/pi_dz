8fce9650336be0a5dae744dc5ef329ae5c78ca2a
Matej Višnjić <visnjicmatej@gmail.com>
2021-10-24 20:11:43 +0200

    ispisivalo je 6 redaka umjesto 5..

--- js_201.js
+++ js_201.js
@@ -1,6 +1,6 @@
 ljestve = "";
-for (let i = 0; i <= 5; i++) {
-  for (let j = 0; j <= i; j++) {
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= i; j++) {
     ljestve += "#";
   }
   ljestve += "\n";
