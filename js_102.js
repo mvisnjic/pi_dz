0428e888c9746814c9a1dd64f4ff57905ea8e531
Matej Višnjić <visnjicmatej@gmail.com>
2021-10-24 19:03:38 +0200

    slucajno mi ostalo hello (eksperimentirao malo)

--- js_102.js
+++ js_102.js
@@ -1,6 +1,6 @@
 let bodovi = prompt("Unesi broj bodova");
 let ocjena;
-let poruka = "hello";
+let poruka;
 // tvoj kod
 if (bodovi < 50) poruka = "pali ste.";
 else if (bodovi > 50 && bodovi <= 60) poruka = "cestitke, ocjena je 2!";
@@ -9,4 +9,3 @@ else if (bodovi > 75 && bodovi <= 85) poruka = "cestitke, ocjena je 4!";
 else if (bodovi > 85 && bodovi <= 100) poruka = "cestitke, ocjena je 5ica!!!";
 else poruka = "krivi unos!";
 console.log(poruka);
-
